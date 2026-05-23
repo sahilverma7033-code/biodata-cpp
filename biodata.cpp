e29cd57360d57948b6e24dc47c3f2e403f539fbd
sahilverma7033-code <sahilverma7033@gmail.com>
2026-05-23 17:09:30 +0530

    Create biodata.cpp

---
+++ biodata.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "=====================================" << endl;
+    cout << "        STUDENT BIODATA FORM         " << endl;
+    cout << "=====================================" << endl;
+    cout << endl;
+    
+    cout << "Name           : Sahil Verma" << endl;
+    cout << "Enrollment No. : 0192CS251148" << endl;
+    cout << "College        : Technocrats Institute of Technology" << endl;
+    cout << "Branch         : B.Tech - Computer Science" << endl;
+    cout << "Year           : 1st Year" << endl;
+    cout << "City          
